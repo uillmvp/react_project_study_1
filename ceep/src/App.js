7fbe496aa7481ae6da0150adcb80a403b1f2925b
uillmvp <uill@outlook.com>
2022-02-01 03:08:13 -0300

    First Commit

---
+++ ceep/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+
+constructor(){
+  super();
+  this.notas = [];  
+
+  this.state = {
+    notas:[]
+  }
+}
+
+
+criarNota(titulo, textarea){
+  const novaNota = { titulo, textarea};
+  const novoArrayNotas = [...this.state.notas,novaNota]
+  const novoEstado = {
+    notas:novoArrayNotas
+  }
+  this.setState(novoEstado);
+}
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro  criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas}/>
+      </section>
+    );
+  }
+}
+
+export default App;
---
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import "./estilo.css";
+class FormularioCadastro extends Component {
+
+  constructor(props){
+    super(props);
+    this.titulo = "";
+    this.textarea = "";
+  }
+
+  _handleMudancaTitulo(evento){
+    evento.stopPropagation();
+    this.titulo = evento.target.value;
+  }
+
+  _handleMudancaTextArea(evento) {
+    evento.stopPropagation();
+    this.textarea = evento.target.value;
+  }
+
+  _criarNota(evento){
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarNota(this.titulo, this.textarea);
+  }
+
+  render() {
+    return (
+      <form className="form-cadastro "
+        onSubmit={this._criarNota.bind(this)}
+      >
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+          onChange={this._handleMudancaTitulo.bind(this)}
+        />
+        <textarea
+          rows={15}
+          placeholder="Escreva sua nota..."
+          className="form-cadastro_input"
+          onChange={this._handleMudancaTextArea.bind(this)}
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar Nota
+        </button>
+      </form>
+    );
+  }
+}
+
+export default FormularioCadastro;
